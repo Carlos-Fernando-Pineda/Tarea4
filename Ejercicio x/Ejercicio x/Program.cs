3871367a5a27b4159aa09564ce1dee25904615b8
Carlos-Fernando-Pineda <79342082+Carlos-Fernando-Pineda@users.noreply.github.com>
2021-03-19 15:05:10 -0600

    Add files via upload

---
+++ Ejercicio x/Ejercicio x/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Ejercicio_x
+{
+    class Program
+    {
+
+        public static string Mid(string parametro, int StartIndex, int length)
+        {
+            string result = parametro.Substring(StartIndex, length);
+            return result;
+        }
+
+        public static void marque(string nombre)
+        {
+            string final;
+            int grua = 0;
+            int finalpos = 0;
+            int inicial1 = 0;
+            int inicial2 = 0;
+            
+            Console.SetCursorPosition(1, 5);
+            Console.Write(nombre);
+            inicial1 = nombre.Length;
+            inicial2 = 50;
+            nombre = nombre.ToUpper();
+
+            for (finalpos = nombre.Length; finalpos >= 1; finalpos--)
+            {
+                final = Mid(nombre, finalpos - 1, 1);
+                for (grua = inicial1; grua <= inicial2; grua++)
+                {
+                    Console.SetCursorPosition(grua, 5);
+                    Console.Write("-" + final);
+                    Thread.Sleep(10);
+                }
+                inicial2 = inicial2 - 1;
+                inicial1 = inicial1 - 1;
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            marque(nombre);
+            marque("juan");
+
+
+        }
+    }
+}
---
+++ Ejercicio-Y/Ejercicio-Y/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_Y
+{
+    class Program
+    {
+
+        public static string GeneraLetra(int rango1, int rango2)
+        {
+            rango1 = 64;
+            rango2 = 123;
+            Random ran = new Random();
+            int numero = Convert.ToInt32(ran.Next(rango1,rango2));
+            string respuesta = "";
+
+            if (((numero > rango1) & (numero < rango2)))
+            {
+                respuesta = Convert.ToString((char)numero);
+            }
+            return respuesta;
+        } 
+
+
+        static void Main(string[] args)
+        {
+            byte CAN, contador;
+            //en el video se me olvido explicar que el contador practicamente va aumentando hasta que se cumple la cantidad dada de numeros que el usuario da, CAN es la cantidad dada
+            string linea;
+            Console.Write("Cuantos Numeros: ");
+            linea = Console.ReadLine();
+            CAN = byte.Parse(linea);
+            string letras = "";
+            for (contador = 1; contador <= CAN; contador++)
+            {
+                letras = GeneraLetra(64, 123);
+                Console.WriteLine($"El codigo de {letras}");
+            }
+        }
+    }
+}
